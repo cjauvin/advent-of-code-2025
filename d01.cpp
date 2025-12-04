0e4ade009011e784353e785a70a1f3cc91190730
Christian Jauvin <cjauvin@gmail.com>
2025-12-04 10:27:09 -0500

    VSC stuff

--- d01.cpp
+++ d01.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <ranges>
 #include <iostream>
+#include <cassert>
 
 int part1(std::vector<std::pair<int,int>>& values) {
     int dial = 50;
@@ -34,9 +35,33 @@ int part2(std::vector<std::pair<int,int>>& values) {
     return n_zeros;
 }
 
+// std::vector<std::string> get_test_data() {
+//     std::string test = R"(L68
+// L30
+// R48
+// L5
+// R60
+// L55
+// L1
+// L99
+// R14
+// L82
+// )";
+//     std::istringstream iss(test);
+//     std::string line;
+//     std::vector<std::string> lines;
+
+//     while (std::getline(iss, line)) {
+//         lines.push_back(std::move(line));
+//     }
+
+//     assert(lines.size() == 10);
+
+//     return lines;
+// }
+
 std::vector<std::string> get_test_data() {
-    std::vector<std::string> lines;
-    std::string test = R"(L68
+    constexpr std::string_view test = R"(L68
 L30
 R48
 L5
@@ -47,14 +72,16 @@ L99
 R14
 L82
 )";
-    std::istringstream iss(test);
-    std::string line;
-
-    while (std::getline(iss, line)) {
-        lines.push_back(std::move(line));
+    std::vector<std::string> out;
+    for (auto part : test | std::views::split('\n')) {
+        if (!std::ranges::empty(part)) {
+            out.emplace_back(part.begin(), part.end());  // make a std::string
+        }
     }
 
-    return lines;
+    assert(out.size() == 10);
+
+    return out;
 }
 
 std::vector<std::string> get_data() {
@@ -74,6 +101,7 @@ int main(int argc, char* argv[]) {
 
     for (const auto& line : lines) {
         if (line.empty()) continue;
+        // std::cout << line << "\n";
         int step = line[0] == 'R' ? 1 : -1;
         int value = std::stoi(line.substr(1));
         values.emplace_back(step, value);
